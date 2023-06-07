f7f27ea3db51459269d36748d6ddbafc0146c121
myeong <audelahd@gmail.com>
2023-06-07 17:37:48 +0900

    객체 지향 프로그래밍과 접근, 생성자 함수 예제

---
+++ mo06/day07/041.js
@@ -0,0 +1,6 @@
+if (true) {
+  var functionScopeValue = "gloval";
+  let blockScopeValue = "local";
+}
+console.log(functionScopeValue);
+//console.log(blockScopeValue);
---
+++ mo06/day07/042.js
@@ -0,0 +1,9 @@
+const URL = "http://js.com";
+URL = "http://js.com";
+
+if (true) {
+  const URL2 = "http://js.com";
+}
+
+//console.log(URL2);
+//if문 안에서 정의된 변수는 if문 밖에서 접근시 에러가 발생한다.
